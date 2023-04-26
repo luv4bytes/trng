c1c131471c57789d4821639842f042c5059452eb
luv4bytes <61706439+luv4bytes@users.noreply.github.com>
2023-04-26 12:14:18 +0200

    License text

--- trng/src/tape/error.rs
+++ trng/src/tape/error.rs
@@ -1,4 +1,18 @@
-use std::array::TryFromSliceError;
+// TRNG - Brainfucks pretty sister.
+// Copyright (C) 2023 Lukas Pfeifer
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 /// Defines error types for tape errors.
 #[derive(Debug)]
@@ -46,16 +60,6 @@ impl From<std::io::Error> for TapeError {
         }
     }
 }
-
-impl From<TryFromSliceError> for TapeError {
-    fn from(value: TryFromSliceError) -> Self {
-        Self {
-            description: value.to_string(),
-            _type: TapeErrorType::Index,
-        }
-    }
-}
-
 impl TapeError {
     pub fn new(_type: TapeErrorType, description: String) -> TapeError {
         Self { _type, description }
